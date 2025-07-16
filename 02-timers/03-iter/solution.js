fcebac42cdfebaf7546a9064e4d808923b6dce69
Kostiantyn <firstpickpudge@gmail.com>
2025-07-16 11:54:27 +0300

    resolved timers iter

---
+++ 02-timers/03-iter/solution.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Task: fix given code to prevent removing items from
+// `electronics` array and to stop iteration after last item
+
+import { setInterval } from "node:timers/promises";
+
+const electronics = [
+  { name: "Laptop", price: 1500 },
+  { name: "Keyboard", price: 100 },
+  { name: "HDMI cable", price: 10 },
+];
+
+const main = async () => {
+  const maxIndex = electronics.length - 1;
+  let currentIndex = 0;
+  const delay = 1000;
+  const iter = setInterval(delay, electronics);
+  for await (const items of iter) {
+    if (currentIndex > maxIndex) {
+      console.log("Counting ended.");
+      break;
+    }
+    console.log({ item: items[currentIndex] });
+    currentIndex++;
+  }
+};
+
+main();
