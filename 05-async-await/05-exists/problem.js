1628d997e3e990aae63bd781c2347585655b841f
Kostiantyn <firstpickpudge@gmail.com>
2025-07-27 15:14:06 +0300

    added async-await tasks

---
+++ 05-async-await/05-exists/problem.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Task: optimize `fileExists` function to one-liner
+// Do not change "Usage" section, edit just "Implementation"
+
+const fs = require('node:fs');
+
+// Implementation
+
+const fileExists = async (name) => {
+  try {
+    await fs.promises.access(name);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+// Usage
+
+const main = async () => {
+  {
+    const name = 'file-name.ext';
+    const exists = await fileExists(name);
+    console.log({ name, exists });
+  }
+
+  {
+    const name = '5-exists-problem.js';
+    const exists = await fileExists(name);
+    console.log({ name, exists });
+  }
+};
+
+main();
